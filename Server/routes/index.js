14aae6761d00e68dd896a9ce4fcbc308afc6df40
QuocDuy03 <100069273+QuocDuy03@users.noreply.github.com>
2023-12-24 15:01:35 +0700

    Delete Server directory

--- Server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const userRouter = require('./user');
-const productRouter = require('./products');
-function route(app) {
-    app.use('/', userRouter);
-    app.use('/', productRouter);
-}
-
-module.exports = route;
